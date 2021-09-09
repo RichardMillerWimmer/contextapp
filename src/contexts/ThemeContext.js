54df4a8233f40e2c11ebd06107f6be91d141f4bf
Richard Miller <richardmmill@gmail.com>
2021-09-09 19:51:37 -0400

    ThemeContext

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -2,16 +2,16 @@ import React, { useState, createContext } from 'react'
 
 export const ThemeContext = createContext();
 
-export const ThemeContextProvider = () => {
-    const [theme] = useState({
+export const ThemeContextProvider = (props) => {
+    const [theme, setTheme] = useState({
         isLightTheme: true,
         light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
         dark: { syntax: '#ddd', ui: '#333', bg: '#555' }
     })
 
     return (
-        <ThemeContext.Provider value={{...theme}}>
-            
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            {props.children}
         </ThemeContext.Provider>
     )
 }
