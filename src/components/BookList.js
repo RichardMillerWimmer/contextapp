27187d236b7ea9ab1ef2427bd8fc5d44727dffa5
Richard Miller <richardmmill@gmail.com>
2021-09-09 19:31:29 -0400

    BookList.js and css added

---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const BookList = () => {
+    return (
+        <div className='book-list'>
+            <ul>
+                <li>Book 1</li>
+                <li>Book 2</li>
+                <li>Book 3</li>
+            </ul>
+        </div>
+    )
+}
