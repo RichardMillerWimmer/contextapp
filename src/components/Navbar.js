63e02c389f29525a8639619a6b07cda931419b1c
Richard Miller <richardmmill@gmail.com>
2021-09-10 11:45:56 -0400

    AuthContext in App.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react'
+import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext } from '../contexts/ThemeContext'
 
 export const Navbar = () => {
     const {theme} = useContext(ThemeContext);
+    const {auth, toggleAuth} = useContext(AuthContext)
 
     const { isLightTheme, light, dark } = theme;
 
@@ -15,6 +17,9 @@ export const Navbar = () => {
     return (
         <nav style={{ background: style.ui, color: style.syntax }}>
             <h1>Context App</h1>
+            <div onClick={toggleAuth}>
+                {auth ? 'Logged in' : 'Logged Out'}
+            </div>
             <ul>
                 <li>Home</li>
                 <li>About</li>
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -10,7 +10,7 @@ export const AuthContextProvider = (props) => {
     }
 
     return (
-        <AuthContext.Provider value={{toggleAuth}}>
+        <AuthContext.Provider value={{auth, toggleAuth}}>
             {props.children}
         </AuthContext.Provider>
     )
