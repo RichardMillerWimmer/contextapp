49e58b7b166fbdd61a4b14424b178c0eda6ba332
Richard Miller <richardmmill@gmail.com>
2021-09-10 10:51:24 -0400

    ThemeToggle.js

---
+++ src/components/ThemeToggle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import React from 'react'
+
+export const ThemeToggle = () => {
+    return (
+        <div>
+            <button onClick={}>Toggle Theme</button>
+        </div>
+    )
+}
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react'
+import React, { useState, createContext, useEffect } from 'react'
 
 export const ThemeContext = createContext();
 
@@ -8,6 +8,18 @@ export const ThemeContextProvider = (props) => {
         light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
         dark: { syntax: '#ddd', ui: '#333', bg: '#555' }
     })
+    function toggleTheme() {
+        const newTheme = {...theme}
+        
+        console.log(newTheme)
+        setTheme({...newTheme})
+        console.log(theme)
+    }
+
+    useEffect(() => {
+        toggleTheme()
+    }, [])
+    
 
     return (
         <ThemeContext.Provider value={[theme, setTheme]}>
